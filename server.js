f860fb09ed0227f680829c5c24763553fb364811
Gyan Pratipat <mymaingyan@gmail.com>
2025-09-19 16:09:10 -0700

    Removed condition to use dotenv

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-// Load environment variables from the .env file only in development
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
+// // Load environment variables from the .env file only in development
+// if (process.env.NODE_ENV !== 'production') {
+//   require('dotenv').config();
+// }
 
 const express = require('express');
 const cors = require('cors');
